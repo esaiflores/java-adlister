dc2df41fa1da739f534c0c372a37aefdcc11c60b
Esai Flores <flores.esaijonathan@gmail.com>
2020-06-08 09:46:57 -0500

    finished exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, IOException {
+        String name = req.getParameter("name");
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        if(name == null || name.isEmpty()){
+            out.println("<h1>Hello world !" + "</h1>");
+        }else{
+            out.println("<h1>Hello " + name + "!</h1>");
+        }
 
     }
 }
